44400a040d437cf1a2fb1ee13a149e26f4ede15c
raphaell <raphaellmsousa@gmail.com>
2020-02-15 07:52:24 -0300

    project 5 home service robot

---
+++ project5/test_navigation.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/raphaell/workspace/catkin_ws/src/worlds/raphaell.world " &
+sleep 5
+#xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=~/catkin_ws/src/worlds/umap.yaml " &
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch " &
+sleep 5
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch "
