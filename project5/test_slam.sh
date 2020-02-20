813f4b0385821e1954ead7c2afaeec34d1973c74
raphaell <raphaellmsousa@gmail.com>
2020-02-20 08:36:39 -0300

    project5

--- project5/test_slam.sh
+++ project5/test_slam.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/raphaell/workspace/catkin_ws/src/worlds/raphaell.world " &
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/raphaell/workspace/catkin_ws/src/worlds/raphaell.world " &
+#xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
 sleep 5
 xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch " &
 sleep 5
