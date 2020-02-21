d0ded0bc6e07a5891d34cd7d1045203d24689652
raphaell <raphaellmsousa@gmail.com>
2020-02-21 20:31:39 -0300

    add setDestination and ros sleep functions

--- project5/pick_objects/src/pick_objects.cpp
+++ project5/pick_objects/src/pick_objects.cpp
@@ -5,6 +5,10 @@
 // Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
+void setDestination(float x, float y, float w);
+
+move_base_msgs::MoveBaseGoal goal;
+
 int main(int argc, char** argv){
   // Initialize the simple_navigation_goals node
   ros::init(argc, argv, "pick_objects");
@@ -17,59 +21,62 @@ int main(int argc, char** argv){
     ROS_INFO("Waiting for the move_base action server to come up");
   }
 
-  move_base_msgs::MoveBaseGoal goal;
-
   // set up the frame parameters
   goal.target_pose.header.frame_id = "map";
   goal.target_pose.header.stamp = ros::Time::now();
 
-  // Define a position and orientation for the robot to reach
-  goal.target_pose.pose.position.x = 6.69;
-  goal.target_pose.pose.position.y = 5.67;
-  goal.target_pose.pose.orientation.w = -0.00143;
+  setDestination(6.69, 5.67, -0.00143);
 
-   // Send the goal position and orientation for the robot to reach
-  ROS_INFO("Sending first goal at x: %f, y: %f, w: %f",
-    goal.target_pose.pose.position.x,
-    goal.target_pose.pose.position.y,
-    goal.target_pose.pose.orientation.w
-    );
-    
   ac.sendGoal(goal);
 
   // Wait an infinite time for the results
   ac.waitForResult();
 
   // Check if the robot reached its goal
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("The robot has reached the first goal");
-  else
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
+    ROS_INFO("The robot has reached the second goal");
+    ros::Duration(5.0).sleep();
+    //std::cin.get(); //we need press enter to move on
+  }
+  else{
     ROS_INFO("The base failed to move for some reason");
+    ros::Duration(5.0).sleep();
+    //std::cin.get();
+  }
 
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-  {
-    // Define a position and orientation for the robot to reach
-    goal.target_pose.pose.position.x = 9.63;
-    goal.target_pose.pose.position.y = 6.53;
-    goal.target_pose.pose.orientation.w = -0.00143;
+  setDestination(9.63, 6.53, -0.00143);
+
+  ac.sendGoal(goal);
+
+  ac.waitForResult();
+  
+  // Check if the robot reached its goal
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
+    ROS_INFO("The robot has reached the second goal");
+    ros::Duration(5.0).sleep();
+    //std::cin.get();
+  }
+  else{
+    ROS_INFO("The base failed to move for some reason");
+    ros::Duration(5.0).sleep();
+    //std::cin.get();
+  }
+    
+
+  return 0;
+}
+
+void setDestination(float x, float y, float w){ 
+
+  // Define a position and orientation for the robot to reach
+  goal.target_pose.pose.position.x = x;
+  goal.target_pose.pose.position.y = y;
+  goal.target_pose.pose.orientation.w = w;
 
    // Send the goal position and orientation for the robot to reach
-    ROS_INFO("Sending secound goal at x: %f, y: %f, w: %f",
-    goal.target_pose.pose.position.x,
-    goal.target_pose.pose.position.y,
-    goal.target_pose.pose.orientation.w
-    );
-    
-    ac.sendGoal(goal);
-
-    // Wait an infinite time for the results
-    ac.waitForResult();
-
-    // Check if the robot reached its goal
-    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-      ROS_INFO("That's all! Final destination!");
-    else
-      ROS_INFO("The base failed to move for some reason");
-    }
-  return 0;
+  ROS_INFO("Sending goal at x: %f, y: %f, w: %f",
+  goal.target_pose.pose.position.x,
+  goal.target_pose.pose.position.y,
+  goal.target_pose.pose.orientation.w
+  );
 }
